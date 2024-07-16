026f2ad1c9c4def746635fc83f2f5f5de89d4546
Laraib Azmat <129507203+Laraib-Azmat@users.noreply.github.com>
2024-07-16 07:33:46 +0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 
 
-const supabaseUrl = 'https://tuizpkzdskdlknfgaplo.supabase.co';
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1aXpwa3pkc2tkbGtuZmdhcGxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ5MDExOTMsImV4cCI6MjAzMDQ3NzE5M30.h_e35rmNz_tj0Q1PI-mbwyjFV2NDv8DfbXwYvbUqfKY";
+const supabaseUrl = '';
+const supabaseAnonKey = "";
 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
